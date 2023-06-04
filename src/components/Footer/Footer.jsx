c7ed791f516fb6319106ca3399c00bdbda07c840
Joss Robson <100178421+JossRobson@users.noreply.github.com>
2023-06-04 00:08:16 -0300

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,11 +2,18 @@
 
 function Footer() {
   return (
-    <div className="text-center bg-warning py-2 mt-5">
+    <div className="text-center bg-secondary py-2 mt-5">
         <p className=" fs-2"></p>
 
+                
+
+        
+
     </div>
+    
+    
   )
 }
+
 
 export default Footer
